48f0d7eed5169111e795cd007e3de3e6e38d8c18
Andrei Alexandru <Andrei Alexandru>
2017-03-04 23:40:10 +0000

    C program test for LED

--- LedBorg/test.c
+++ LedBorg/test.c
@@ -1,10 +1,16 @@
 #include <stdio.h>    // Used for printf() statements
 #include <wiringPi.h> // Include WiringPi library!
-
+#include <time.h>
 // Pin number declarations. We're using the Broadcom chip pin numbers.
 const int redPin = 17; // red green - Broadcom pin 18, P1 pin 12
 const int greenPin = 27; // Regular green - Broadcom pin 23, P1 pin 16
 const int bluePin = 22; // Active-low blueton - Broadcom pin 17, P1 pin 11
+srand(time(NULL));   // should only be called once
+int r = rand();      // returns a pseudo-random integer between 0 and RAND_MAX
+int ok = 2;
+int p = 0;
+int myTime = 0;
+
 
 int main(void)
 {
@@ -25,49 +31,101 @@ int main(void)
     // Loop (while(1)):
     while(1)
     {
+        if (ok == 2){
+            ok = 0;
+            myTime = 540;
+        } else {
+            ok++;
+            myTime = 270;
+        }
 
-        digitalWrite(redPin, HIGH);
-        digitalWrite(bluePin, HIGH);
+        p = r % 3;
 
-        delay(400);
+        if (p == 0){
 
-        digitalWrite(redPin, LOW);
-        digitalWrite(bluePin, LOW);
+            p = r % 3;
+            
+            if(p == 0){
 
-        digitalWrite(redPin, HIGH);
+                digitalWrite(redPin, HIGH);
+                delay(myTime);
+                digitalWrite(redPin, LOW);
 
-        delay(200);
+            } else if (p == 1 ){
 
-        digitalWrite(redPin, LOW);
- 
-        digitalWrite(redPin, HIGH);
-        digitalWrite(greenPin, HIGH);
+                digitalWrite(greenPin, HIGH);
+                digitalWrite(redPin, HIGH);
+                delay(myTime);
+                digitalWrite(redPin, LOW);
+                digitalWrite(greenPin, LOW);
 
-        delay(200);
+            } else {
 
-        digitalWrite(redPin, LOW);
-        digitalWrite(greenPin, LOW);
+                digitalWrite(bluePin, HIGH);
+                digitalWrite(redPin, HIGH);
+                delay(myTime);
+                digitalWrite(bluePin, LOW);
+                digitalWrite(redPin, LOW);
+            
+            }
+        } else if (p == 1){
+            
+            p = r % 3;
+            
+            if(p == 0){
 
-        digitalWrite(redPin, HIGH);
-        digitalWrite(bluePin, HIGH);
+                digitalWrite(greenPin, HIGH);
+                digitalWrite(redPin, HIGH);
+                delay(myTime);
+                digitalWrite(redPin, LOW);
+                digitalWrite(greenPin, LOW);
 
-        delay(400);
+            } else if (p == 1 ){
 
-        digitalWrite(redPin, LOW);
-        digitalWrite(bluePin, LOW);
+                digitalWrite(greenPin, HIGH);
+                delay(myTime);
+                digitalWrite(greenPin, LOW);
 
-        digitalWrite(bluePin, HIGH);
+            } else {
 
-        delay(200);
+                digitalWrite(bluePin, HIGH);
+                digitalWrite(greenPin, HIGH);
+                delay(myTime);
+                digitalWrite(bluePin, LOW);
+                digitalWrite(greenPin, LOW);
+            
+            }
 
-        digitalWrite(bluePin, LOW);
+        } else {
+            
+            p = r % 3;
+            
+            if(p == 0){
 
-        digitalWrite(bluePin, HIGH);
-        digitalWrite(greenPin, HIGH);
+                digitalWrite(bluePin, HIGH);
+                digitalWrite(redPin, HIGH);
+                delay(myTime);
+                digitalWrite(redPin, LOW);
+                digitalWrite(bluePin, LOW);
 
-        delay(200);
-        digitalWrite(greenPin, LOW);
-        digitalWrite(bluePin, LOW);
+            } else if (p == 1 ){
+
+                digitalWrite(bluePin, HIGH);
+                digitalWrite(greenPin, HIGH);
+                delay(myTime);
+                digitalWrite(greenPin, LOW);
+                digitalWrite(bluePin, LOW);
+
+            } else {
+
+                digitalWrite(bluePin, HIGH);
+                delay(myTime);
+                digitalWrite(bluePin, LOW);
+            
+            }
+
+        }
+        
 
     }
 
