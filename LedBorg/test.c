2452eb8ab7c7a288f792c7c3aa7032a7d37adbdc
Andrei Alexandru <Andrei Alexandru>
2017-03-04 22:08:58 +0000

    added a small sequence to play

--- LedBorg/test.c
+++ LedBorg/test.c
@@ -2,41 +2,58 @@
 #include <wiringPi.h> // Include WiringPi library!
 
 // Pin number declarations. We're using the Broadcom chip pin numbers.
-const int pwmPin = 18; // PWM LED - Broadcom pin 18, P1 pin 12
-const int ledPin = 23; // Regular LED - Broadcom pin 23, P1 pin 16
-const int butPin = 17; // Active-low button - Broadcom pin 17, P1 pin 11
-
-const int pwmValue = 75; // Use this to set an LED brightness
+const int pwmPin = 17; // PWM LED - Broadcom pin 18, P1 pin 12
+const int ledPin = 21; // Regular LED - Broadcom pin 23, P1 pin 16
+const int butPin = 22; // Active-low button - Broadcom pin 17, P1 pin 11
 
 int main(void)
 {
     // Setup stuff:
     wiringPiSetupGpio(); // Initialize wiringPi -- using Broadcom pin numbers
 
-    pinMode(pwmPin, PWM_OUTPUT); // Set PWM LED as PWM output
+    pinMode(pwmPin, OUTPUT); // Set PWM LED as PWM output
     pinMode(ledPin, OUTPUT);     // Set regular LED as output
-    pinMode(butPin, INPUT);      // Set button as INPUT
-    pullUpDnControl(butPin, PUD_UP); // Enable pull-up resistor on button
+    pinMode(butPin, OUTPUT);      // Set button as INPUT
+
+    digitalWrite(pwmPin, LOW);
+    digitalWrite(ledPin, LOW);
+    digitalWrite(butPin, LOW);
+    
 
     printf("Blinker is running! Press CTRL+C to quit.\n");
 
     // Loop (while(1)):
     while(1)
     {
-        if (digitalRead(butPin)) // Button is released if this returns 1
-        {
-            pwmWrite(pwmPin, pwmValue); // PWM LED at bright setting
-            digitalWrite(ledPin, LOW);     // Regular LED off
-        }
-        else // If digitalRead returns 0, button is pressed
-        {
-            pwmWrite(pwmPin, 1024 - pwmValue); // PWM LED at dim setting
-            // Do some blinking on the ledPin:
-            digitalWrite(ledPin, HIGH); // Turn LED ON
-            delay(75); // Wait 75ms
-            digitalWrite(ledPin, LOW); // Turn LED OFF
-            delay(75); // Wait 75ms again
-        }
+ 
+        digitalWrite(pwmPin, HIGH);
+        digitalWrite(ledPin, HIGH);
+        digitalWrite(butPin, HIGH);
+
+        delay(400);
+
+        digitalWrite(pwmPin, LOW);
+        digitalWrite(ledPin, LOW);
+        digitalWrite(butPin, LOW);
+
+        digitalWrite(pwmPin, HIGH);
+
+        delay(200);
+
+        digitalWrite(pwmPin, LOW);
+
+        digitalWrite(ledPin, HIGH);
+
+        delay(150);
+
+        digitalWrite(ledPin, LOW);
+
+        digitalWrite(butPin, HIGH);
+
+        delay(100);
+
+        digitalWrite(butPin, LOW);
+
     }
 
     return 0;
