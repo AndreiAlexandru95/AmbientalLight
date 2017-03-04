62427b55d87d15d31b5e450267e3f4713c8f60c4
Andrei Alexandru <Andrei Alexandru>
2017-03-04 22:24:45 +0000

    test - updated with new pattern

--- LedBorg/test.c
+++ LedBorg/test.c
@@ -2,22 +2,22 @@
 #include <wiringPi.h> // Include WiringPi library!
 
 // Pin number declarations. We're using the Broadcom chip pin numbers.
-const int pwmPin = 17; // PWM LED - Broadcom pin 18, P1 pin 12
-const int ledPin = 27; // Regular LED - Broadcom pin 23, P1 pin 16
-const int butPin = 22; // Active-low button - Broadcom pin 17, P1 pin 11
+const int redPin = 17; // red green - Broadcom pin 18, P1 pin 12
+const int greenPin = 27; // Regular green - Broadcom pin 23, P1 pin 16
+const int bluePin = 22; // Active-low blueton - Broadcom pin 17, P1 pin 11
 
 int main(void)
 {
     // Setup stuff:
     wiringPiSetupGpio(); // Initialize wiringPi -- using Broadcom pin numbers
 
-    pinMode(pwmPin, OUTPUT); // Set PWM LED as PWM output
-    pinMode(ledPin, OUTPUT);     // Set regular LED as output
-    pinMode(butPin, OUTPUT);      // Set button as INPUT
+    pinMode(redPin, OUTPUT); // Set red green as red output
+    pinMode(greenPin, OUTPUT);     // Set regular green as output
+    pinMode(bluePin, OUTPUT);      // Set blueton as INPUT
 
-    digitalWrite(pwmPin, LOW);
-    digitalWrite(ledPin, LOW);
-    digitalWrite(butPin, LOW);
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(bluePin, LOW);
     
 
     printf("Blinker is running! Press CTRL+C to quit.\n");
@@ -25,34 +25,49 @@ int main(void)
     // Loop (while(1)):
     while(1)
     {
- 
-        digitalWrite(pwmPin, HIGH);
-        digitalWrite(ledPin, HIGH);
-        digitalWrite(butPin, HIGH);
+
+        digitalWrite(redPin, HIGH);
+        digitalWrite(bluePin, HIGH);
 
         delay(400);
 
-        digitalWrite(pwmPin, LOW);
-        digitalWrite(ledPin, LOW);
-        digitalWrite(butPin, LOW);
+        digitalWrite(redPin, LOW);
+        digitalWrite(bluePin, LOW);
 
-        digitalWrite(pwmPin, HIGH);
+        digitalWrite(redPin, HIGH);
 
         delay(200);
 
-        digitalWrite(pwmPin, LOW);
+        digitalWrite(redPin, LOW);
+ 
+        digitalWrite(redPin, HIGH);
+        digitalWrite(greenPin, HIGH);
 
-        digitalWrite(ledPin, HIGH);
+        delay(200);
 
-        delay(150);
+        digitalWrite(redPin, LOW);
+        digitalWrite(greenPin, LOW);
 
-        digitalWrite(ledPin, LOW);
+        digitalWrite(redPin, HIGH);
+        digitalWrite(bluePin, HIGH);
 
-        digitalWrite(butPin, HIGH);
+        delay(400);
 
-        delay(100);
+        digitalWrite(redPin, LOW);
+        digitalWrite(bluePin, LOW);
 
-        digitalWrite(butPin, LOW);
+        digitalWrite(bluePin, HIGH);
+
+        delay(200);
+
+        digitalWrite(bluePin, LOW);
+
+        digitalWrite(bluePin, HIGH);
+        digitalWrite(greenPin, HIGH);
+
+        delay(200);
+        digitalWrite(greenPin, LOW);
+        digitalWrite(bluePin, LOW);
 
     }
 
