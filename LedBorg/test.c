be95fc7f28cef70ab30fb51652a27a37b94565c7
Andrei Alexandru <aa15g14@soton.ac.uk>
2017-04-28 09:03:10 +0300

    Added some debug messages

--- LedBorg/test.c
+++ LedBorg/test.c
@@ -53,9 +53,10 @@ void getColor(void){
     int color;
     FILE *colorFile;
     colorFile = fopen("../colorFile.txt", "r");
+    printf(colorFile);
     if (colorFile){
         while((color = getc(colorFile)) != EOF){
-
+            printf("The color read from the file is: %d", color);
             switch(color){
                 case 0:{
                     digitalWrite(redPin, HIGH);
